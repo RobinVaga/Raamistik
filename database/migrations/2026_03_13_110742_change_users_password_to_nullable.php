ee8f64b62428fc0d1a0c5194d675dd783dc94078
Robin <robin.vaga@ametikool.ee>
2026-03-26 14:34:59 +0200

    same

--- database/migrations/2026_03_13_110742_change_users_password_to_nullable.php
+++ database/migrations/2026_03_13_110742_change_users_password_to_nullable.php
@@ -23,7 +23,6 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('password')->change();
-            $table->dropColumn('google_id');
         });
     }
 };
