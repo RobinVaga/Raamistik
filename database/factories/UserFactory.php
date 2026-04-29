25f7a6b93eab83c22d75b65d9751c5b8712dd9c3
Robin <robin.vaga@ametikool.ee>
2026-04-29 15:32:34 +0300

    Fixes to UserFactory once again.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,8 +19,8 @@ class UserFactory extends Factory
         $faker = FakerFactory::create();
 
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
